8a5b6462825f020348bcc2ff7f882e45b85d4147
Bivek Gurung <bivek.tamu@gmail.com>
2024-05-27 13:15:59 +1000

    Testing recaptcha. bug exist

--- js/submit-form.js
+++ js/submit-form.js
@@ -13,7 +13,6 @@ $(function () {
     if (!form[0].checkValidity()) {
       event.preventDefault();
       event.stopPropagation();
-      console.log("asdf");
     } else {
       event.preventDefault();
       $(".submit_form").html("Sending...");
@@ -21,28 +20,28 @@ $(function () {
       const toast = new bootstrap.Toast($(".success_msg")[0]);
       const errtoast = new bootstrap.Toast($(".error_msg")[0]);
       var formData = new FormData(event.target);
-      console.log(JSON.stringify(formData));
-      let jsonstr = {}
+      let jsonstr = {};
       for (const [key, value] of formData) {
         // console.log(key, value);
-        jsonstr[key] = value
+        jsonstr[key] = value;
       }
       console.log(jsonstr);
+      if (!jsonstr["g-recaptcha-response"]) {
+        return $(".recaptcha.invalid-feedback").show();
+      }
 
-      return
       fetch(
         "https://public.herotofu.com/v1/422e00c0-c028-11ee-891f-6d871096fc6f",
         {
           method: "POST",
           mode: "no-cors",
-          body: formData,
+          body: JSON.stringify(jsonstr),
         }
       )
         .then((response) => {
           // Note: You won't be able to access the response body when mode is 'no-cors'
           console.log(response);
           console.log("Request made with no-cors mode");
-
           toast.show();
           $(".submit_form").html("Send Message");
         })
