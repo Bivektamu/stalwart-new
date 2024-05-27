f0c1c050ac29974e810df0a6ca655a898f365095
Bivek Gurung <bivek.tamu@gmail.com>
2024-05-27 14:34:03 +1000

    form submision done

--- js/submit-form.js
+++ js/submit-form.js
@@ -13,44 +13,32 @@ $(function () {
     for (const [key, value] of formData) {
       jsonstr[key] = value;
     }
-
     if (!form[0].checkValidity()) {
       event.preventDefault();
       event.stopPropagation();
-    } else if (!jsonstr["g-recaptcha-response"]) {
+    } 
+    else if (!jsonstr["g-recaptcha-response"]) {
       event.preventDefault();
       event.stopPropagation();
        $(".recaptcha.invalid-feedback").show();
+    } 
 
-    } else {
-      $(".submit_form").html("Sending...");
-    }
-
-    /*
     else {
       event.preventDefault();
-
-      var formData = new FormData(event.target);
-      let jsonstr = {};
-      for (const [key, value] of formData) {
-        // console.log(key, value);
-        jsonstr[key] = value;
-      }
-      // if (!jsonstr["g-recaptcha-response"]) {
-      //   return $(".recaptcha.invalid-feedback").show();
-      // }
-
       $(".submit_form").html("Sending...");
       $(".btn_submit-subscribe").html("Sending...");
       const toast = new bootstrap.Toast($(".success_msg")[0]);
       const errtoast = new bootstrap.Toast($(".error_msg")[0]);
-
+      delete jsonstr["g-recaptcha-response"]
       console.log(JSON.stringify(jsonstr));
 
       fetch(
         "https://public.herotofu.com/v1/422e00c0-c028-11ee-891f-6d871096fc6f",
         {
           method: "POST",
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+          },
           body: JSON.stringify(jsonstr),
         }
       )
@@ -69,8 +57,6 @@ $(function () {
           $(".btn_submit-subscribe").html("SUBSCRIBE NOW");
         });
     }
-
-     */
 
     form.addClass("was-validated");
   });
